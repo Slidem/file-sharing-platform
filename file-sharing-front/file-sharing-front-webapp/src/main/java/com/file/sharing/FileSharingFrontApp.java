24ffa0c0867307a96fb40b7af9a45cfd192a25c5
Aioanei Andrei <aio.andrei@gmail.com>
2017-09-16 05:40:33 +0300

    First commit local

---
+++ file-sharing-front/file-sharing-front-webapp/src/main/java/com/file/sharing/FileSharingFrontApp.java
@@ -0,0 +1,12 @@
+package com.file.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingFrontApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FileSharingFrontApp.class, args);
+	}
+}
